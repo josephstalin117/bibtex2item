a29dd7e643fde70e4649c6a8f8e40f1e0a5cffad
josephlin <josephlin117@hotmail.com>
2022-04-08 00:01:02 +0800

    update parse_bibs & parse_article & parse_authors & bibtex2bibitem

--- bibtex2item.py
+++ bibtex2item.py
@@ -30,121 +30,119 @@ def parse_bibs(filename):
     return bib_list
 
 def parse_article(r):
-    entry_type = re.search(r'@(.*?){.*?', r).group(1)
+    article = {"entry_type":"",
+               "cation_key":"",
+                "title":"",
+                "author":"",
+                "journal":"",
+                "volume":"",
+                "number":"",
+                "pages":"",
+                "year":"",
+                "publisher":"",
+               }
+    article["entry_type"] = re.search(r'@(.*?){.*?', r).group(1)
     search_result = re.search(r'(.*?),(.*?$)', r)
-    cation_key = re.search(r'{(.*?)$', search_result.group(1)).group(1)
+    article["cation_key"] = re.search(r'{(.*?)$', search_result.group(1)).group(1)
     content = search_result.group(2)
-    title = re.search(r'title={(.*?)},',content).group(1)
-    authors = re.search(r'author={(.*?)},',content).group(1)
-    journal = re.search(r'journal={(.*?)},',content).group(1)
-    volume = re.search(r'volume={(.*?)},',content).group(1)
-    number = re.search(r'number={(.*?)},',content).group(1)
-    pages = re.search(r'pages={(.*?)},',content).group(1)
-    year = re.search(r'year={(.*?)},',content).group(1)
-    publisher = re.search(r'publisher={(.*?)},',content).group(1)
-    print(title)
-    exit()
-    r = r.split('},')
-    for i in r:
-        print(i) 
-    title = venue = volume = number = pages = year = publisher = month = authors = None
-    i = 0
-    for i in r:
-        if '@' == i[0]:
-        #if i.startswith("title"):
+    for key, value in article.items():
+        m = re.search(key + r'={(.*?)},',content)
+        if m:
+            article[key] = m.group(1)
+    
+    article["author"] = parse_authors(article["author"])
 
-            code = line.split('{')[-1][:-1]
-            title = venue = volume = number = pages = year = publisher = month = authors = None
-            output_authors = []
-            i += 1
-    while i < len(r) and '@' not in r[i]:
-        line = r[i].strip()
-        #print(line)
-        if line.startswith("title"):
-            title = line.split('{')[-1][:-2]
-        elif line.startswith("journal"):
-            venue = line.split('{')[-1][:-2]
-        elif line.startswith("volume"):
-            volume = line.split('{')[-1][:-2]
-        elif line.startswith("number"):
-            number = line.split('{')[-1][:-2]
-        elif line.startswith("pages"):
-            pages = line.split('{')[-1][:-2]
-        elif line.startswith("year"):
-            year = line.split('{')[-1][:-2]
-        elif line.startswith("publisher"):
-            publisher = line.split('{')[-1][:-2]
-        elif line.startswith("author"):
-            authors = line[line.find("{")+1:line.rfind("}")]
-            for LastFirst in authors.split('and'):
-                lf = LastFirst.replace(' ', '').split(',')
-            if len(lf) != 2: continue
-            last, first = lf[0], lf[1]
-            output_authors.append("{}, {}.".format(last.capitalize(), first.capitalize()[0]))
-        i += 1
+    return article
 
 
-def parse_conference(bibtex):
-    bibtex = bibtex.readlines()
-    # print(bibtex)
-    # r = bibtex.split('\n')
-    r = bibtex
-    i = 0
-    while i < len(r):
-        line = r[i].strip()
-    if not line: i += 1
-    if '@' == line[0]:
-        code = line.split('{')[-1][:-1]
-        title = venue = volume = number = pages = year = publisher = month = authors = None
-        output_authors = []
-        i += 1
-    while i < len(r) and '@' not in r[i]:
-        line = r[i].strip()
-        #print(line)
-        if line.startswith("title"):
-            title = line.split('{')[-1][:-2]
-        elif line.startswith("journal"):
-            venue = line.split('{')[-1][:-2]
-        elif line.startswith("volume"):
-            volume = line.split('{')[-1][:-2]
-        elif line.startswith("number"):
-            number = line.split('{')[-1][:-2]
-        elif line.startswith("pages"):
-            pages = line.split('{')[-1][:-2]
-        elif line.startswith("year"):
-            year = line.split('{')[-1][:-2]
-        elif line.startswith("publisher"):
-            publisher = line.split('{')[-1][:-2]
-        elif line.startswith("author"):
-            authors = line[line.find("{")+1:line.rfind("}")]
-            for LastFirst in authors.split('and'):
-                lf = LastFirst.replace(' ', '').split(',')
-            if len(lf) != 2: continue
-            last, first = lf[0], lf[1]
-            output_authors.append("{}, {}.".format(last.capitalize(), first.capitalize()[0]))
-        i += 1
-
-
-def bibtex2bibiterm(bibtex):
-    print("\\bibitem{%s}" % code)
-    if len(output_authors) == 1:
-        print(output_authors[0] + " {}. ".format(title),)
+def parse_authors(authors):
+    authors_list = []
+    output_authors = ""
+    for LastFirst in authors.split('and'):
+        lf = LastFirst.replace(' ', '').split(',')
+        if len(lf) != 2: continue
+        last, first = lf[0], lf[1]
+        authors_list.append("{}. {}".format(first.capitalize()[0], last.capitalize()))
+    if len(authors_list) == 1:
+        output_authors = authors_list[0]
     else:
-        print(", ".join(_ for _ in output_authors[:-1]) + " & " + output_authors[-1] + " {}. ".format(title),)
-    if venue:
-        print ("{{\\em {}}}.".format(" ".join([_.capitalize() for _ in venue.split(' ')])),)
-    if volume:
-        sys.stdout.write(" \\textbf{{{}}}".format(volume))
-    if pages:
-        sys.stdout.write(", {}".format(pages) if number else " pp. {}".format(pages))
-    if year:
-        sys.stdout.write(" ({})".format(year))
-    if publisher and not venue:
-      print("({},{})".format(publisher, year))
+        output_authors = ", ".join(_ for _ in authors_list[:-1]) + " and " + authors_list[-1]
+    return output_authors
+
+
+def parse_conference(r):
+    conference = {"entry_type":"",
+                "cation_key":"",
+                "title":"",
+                "author":"",
+                "journal":"",
+                "volume":"",
+                "number":"",
+                "pages":"",
+                "year":"",
+                "publisher":"",
+               }
+    conference["entry_type"] = re.search(r'@(.*?){.*?', r).group(1)
+    search_result = re.search(r'(.*?),(.*?$)', r)
+    conference["cation_key"] = re.search(r'{(.*?)$', search_result.group(1)).group(1)
+    content = search_result.group(2)
+    for key, value in conference.items():
+        m = re.search(key + r'={(.*?)},',content)
+        if m:
+            conference[key] = m.group(1)
+    
+    conference["author"] = parse_authors(conference["author"])
+
+
+def bibtex2bibitem(bibtex):
+    bibitems = []
+    bibitem = ""
+
+    bibitem = bibtex["author"] + ', ``{}". '.format(bibtex["title"])
+    print(bibitem)
+    if bibtex["journal"]:
+        bibitem = bibitem + ' \emph{' + bibtex["journal"] + '}, '
+    if bibtex['volume']:
+        bibitem = bibitem + 'vol. ' + bibtex["volume"] + ', '
+    if bibtex["number"]:
+        bibitem = bibitem + 'no. ' + bibtex["number"] + ', '
+    if bibtex["pages"]:
+        bibitem = bibitem + 'pp. ' + bibtex["pages"] + ', '
+    if bibtex["year"]:
+        bibitem = bibitem + bibtex["year"]
+    return bibitem
+
+
+def parse_conference(r):
+    conference = {"entry_type":"",
+                "cation_key":"",
+                "title":"",
+                "author":"",
+                "journal":"",
+                "volume":"",
+                "number":"",
+                "pages":"",
+                "year":"",
+                "publisher":"",
+               }
+    conference["entry_type"] = re.search(r'@(.*?){.*?', r).group(1)
+    search_result = re.search(r'(.*?),(.*?$)', r)
+    conference["cation_key"] = re.search(r'{(.*?)$', search_result.group(1)).group(1)
+    content = search_result.group(2)
+    for key, value in conference.items():
+        m = re.search(key + r'={(.*?)},',content)
+        if m:
+            conference[key] = m.group(1)
+    
+    conference["author"] = parse_authors(conference["author"])
+
 
 
 if __name__ == '__main__':
     bibs = parse_bibs("./gatattn.bib")
     #print(bibs)
-    parse_article(bibs[0])
+    article = parse_article(bibs[0])
+    print(article)
+    bibitem = bibtex2bibitem(article)
+    print(bibitem)
     
